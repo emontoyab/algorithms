a4caff96f9b50cbb206dd7d7b98a4d8ed7f051f9
Esteban Montoya <emontoyab93@gmail.com>
2024-06-11 00:11:28 -0500

    Add pop method and tests

--- Tarea_Stack/Stack.ts
+++ Tarea_Stack/Stack.ts
@@ -21,13 +21,54 @@ class Stack<T>{
         if (!this.head) {
             this.head = node
         } else{
-            this.head.next = node
+            node.next = this.head
             this.head = node
         }
-        
+        this.length += 1
     }
 
-    pop(){
+    pop(): T | null{
+        if(!this.head){
+            return null
+        }
+
+        const temp = this.head
+        this.head = this.head.next
+
+        temp.next = null
+        this.length -= 1
+
+        return temp.value
 
     }
 }
+
+//------------------------------------------------------------------------------------
+// Prueba
+const pila = new Stack<number>()
+console.log(pila)   //Stack { head: null, length: 0 }
+
+const nodeA = new StackNode<number>(26)
+pila.push(nodeA)
+console.log(pila)   //Stack { head: StackNode { value: 26, next: null }, length: 1 }
+
+const nodeB = new StackNode<number>(54)
+pila.push(nodeB)
+console.log(pila)   //Stack {head: StackNode { value: 54, next: StackNode { value: 26, next: null } }, length: 2}
+
+const nodeC = new StackNode<number>(5)
+pila.push(nodeC)
+console.log(JSON.stringify(pila)) //{"head":{"value":5,"next":{"value":54,"next":{"value":26,"next":null}}},"length":3}
+
+// pila.pop()
+console.log(pila.pop()) //5
+console.log(pila)   //Stack {head: StackNode { value: 54, next: StackNode { value: 26, next: null } }, length: 2 }
+
+console.log(pila.pop()) //54
+console.log(pila)   //Stack { head: StackNode { value: 26, next: null }, length: 1 }
+
+console.log(pila.pop()) //26
+console.log(pila)   //Stack { head: null, length: 0 }
+
+console.log(pila.pop()) //null
+console.log(pila)   //Stack { head: null, length: 0 }
